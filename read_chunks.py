43b5aaf581a45f5d37d0256c45353226941ab9e2
abhash behera <abhashbehera235@gmail.com>
2026-04-28 19:36:31 +0530

    converting json file to embeddings

--- read_chunks.py
+++ read_chunks.py
@@ -1,15 +1,39 @@
 import requests
+import os
+import json
+import pandas as pd
+# create embeddings of the text
 
-
-def create_embeddings(text):
-    r = requests.port("http://localhost:11434/api/embeddings",json={
+def create_embeddings(text_list):
+    r = requests.post("http://localhost:11434/api/embed",json={
         "model":"bge-m3",
-        "prompt":text
+        "input":text_list
     })
 
-    embedding=r.json()["embedding"]
+    embedding= r.json()["embeddings"]
     return embedding
 
+my_dict=[]
+chunk_id=0
+jsons=os.listdir("jsons") #list all the jsons
 
-a = create_embeddings("cat sat on the mat")
-print(a)
+for json_file in jsons:
+    with open(f"jsons/{json_file}") as f:
+        content=json.load(f)
+        print(f"creating embeddings for {json_file}")
+        
+    embeddings = create_embeddings([c["text"] for c in content["chunks"]]) # first it give a list of texts of chunk , then call the function for embedding
+    for i,chunk in enumerate(content["chunks"]):
+        chunk["chunk_id"] = chunk_id
+        chunk["embedding"] = embeddings[i]
+        chunk_id +=1
+        my_dict.append(chunk)
+
+# print(my_dict)
+
+df = pd.DataFrame.from_records(my_dict)
+print(df)
+
+# a = create_embeddings(["cat sat on the mat","abhash dances on a mat"])
+# print(a)
+ 
